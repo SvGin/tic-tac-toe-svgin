49029a2f4dc1bae1c81f4f746d6272bfbecad3eb
SvGin <>
2024-02-02 23:16:02 +0000

    Changes done afer validation in PEP8

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ def introduction():
     print("1. Game is played on a 3x3 board")
     print("2. Players choose the X or O for their symbol")
     print("3. Players take turns to choose the cell on the game board")
-    print("4. Player who will place three of their symbols in a row , column or dioganal first wins")
-    print("5. If the entire board is filled and there is no winner , the game is a draw.\n")
+    print("4. Place three of the symbols in a row, column or dioganal and win")
+    print("5. If the entire board is filled, the game is a draw.\n")
     print("Good Luck! :)\n")
 
 
@@ -26,8 +26,7 @@ def player_symbols():
         player2 = "O"
     else:
         player2 = "X"
-
-    return player1, player2   
+    return player1, player2
 
 
 def print_board(board):
@@ -47,7 +46,7 @@ def out_of_board(row, col):
     """
     return 0 <= row <= 2 and 0 <= col <= 2
 
-    
+
 def player_move(player):
     """
     Player to imput row and column for the move
@@ -57,15 +56,15 @@ def player_move(player):
     """
     while True:
         try:
-            row = int(input(f"Player {player}, please enter row (0, 1, or 2)\n"))
-            col = int(input(f"Player {player}, please enter column (0, 1, or 2)\n"))
+            row = int(input(f"Player {player}, enter row (0, 1, or 2)\n"))
+            col = int(input(f"Player {player}, enter column (0, 1, or 2)\n"))
 
             if out_of_board(row, col):
                 return row, col
             else:
                 print("Out of range. Please choose beween 0 and 2\n")
         except ValueError:
-            print("Invalid input. Please enter a number 0, 1 or 2. Please try again!\n")
+            print("Invalid input. Please enter 0, 1 or 2. Please try again!\n")
 
 
 def move(board, player, row, col):
@@ -81,13 +80,12 @@ def move(board, player, row, col):
         new_row, new_col = player_move(player)
         move(board, player, new_row, new_col)
 
-    
+
 def player_swap(current_player, player_first, player_second):
     """
     Players to take turns after move registerd on the board
     """
     return player_second if current_player == player_first else player_first
-
 
 
 def full_board(board):
@@ -121,13 +119,12 @@ def if_winner(board):
         return board[0][2]
 
     return None
-    
-            
 
 
 def play_game():
     """
-    This function contains all other needed functions to start and play the game
+    This function contains all other needed functions
+    to start and play the game
     """
     introduction()
     player1, player2 = player_symbols()
@@ -140,7 +137,6 @@ def play_game():
         move(board, current_player, row, col)
         winner = if_winner(board)
 
-
         if winner:
             print_board(board)
             print(f"Player {winner} wins!")
@@ -152,10 +148,7 @@ def play_game():
             print("Its a tie!")
 
             break
-            
         current_player = player_swap(current_player, player1, player2)
 
 
-
-#Call main funcion, start the game
 play_game()
