fc7f5c19e42ec3e62f53ff4209a9c2a38409bedd
SvGin <>
2024-02-02 13:39:20 +0000

    Add function introduction with rules and welcome message

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
+def introduction():
+    """
+    This function contains greating and rules of the game
+    """
+    print("Welcome to the game Tic-Tac-Toe!\n")
+    print("Please read rules of the game: \n")
+    print("1. Game is played on a 3x3 board")
+    print("2. Players choose the X or O for their symbol")
+    print("3. Players take turns to choose the cell on the game board")
+    print("4. Player who will place three of their symbols in a row , column or dioganal first wins")
+    print("5. If the entire board is filled and there is no winner , the game is a draw.\n")
+    print("Good Luck! :)\n")
+
+
 
 def play_game():
     """
     This function contains all other needed functions to start and play the game
     """
+    introduction()
 
 
 #Call main funcion, start the game
